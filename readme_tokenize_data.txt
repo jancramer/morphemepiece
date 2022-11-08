8a1e2c3b26909d90caa9a002ca8df4e47d2ec3e0
McBattery <61551286+McBattery@users.noreply.github.com>
2022-11-08 13:36:44 +0100

    added batch files and readme text file to download text corpus automatically

---
+++ readme_tokenize_data.txt
@@ -0,0 +1,7 @@
+1. execute ./download_openwebtext.bat 
+	- downloads the openwebtext corpus
+2. execute ./unpack_files.bat
+	- unpacks downloaded corpus
+3. execute ./tokenize_data.py
+	- num_process: number of processes created for the tokenization process
+^	- tokenize_text: funtion to tokenize one subset
--- tokenize_data.py
+++ tokenize_data.py
@@ -4,28 +4,38 @@ import datetime
 from tokenizer import MorphemepieceTokenizer
 from multiprocessing import Pool
 from transformers import BertTokenizer
-def tokenize_text(data):
-    print(data)
+def tokenize_text(subset):
+    subset_str=str(subset)
+    if subset < 10:
+        subset_str="0"+str(subset)
     tokenizer=MorphemepieceTokenizer()
     cur_path=os.path.dirname(__file__)
-    data_path=os.path.relpath('..\\openwebtext\\OpenWebText\\subsets\\openwebtext\\urlsf_subset00-'+str(data)+'_data.xz', cur_path)
-    load_data=datetime.datetime.now().time()
-    text=lzma.open(data_path, mode='rt', encoding='utf-8').read()
-    tokenized_text=tokenizer.tokenize(text)
-    with open("C:\\Users\\janch\\OneDrive\\Desktop\\Uni\\BachelorThesis\\tokenized_files\\urlsf_subset00-"+str(data)+"_data_tokenized.txt", 'w', encoding='utf-8') as fp:
-        fp.write(" ".join(token for token in tokenized_text))
-    return data
+    for data in range (1,1001):
+        data_path=os.path.relpath('./openwebtext/urlsf_subset'+subset_str+'-'+str(data)+'_data.xz', cur_path)
+        text=lzma.open(data_path, mode='rt', encoding='utf-8').read()
+        tokenized_text=tokenizer.tokenize(text)
+        with open("./tokenized_data/urlsf_subset"+subset_str+"-"+str(data)+"_data_tokenized.txt", 'w', encoding='utf-8') as fp:
+
+            fp.write(" ".join(token for token in tokenized_text))
+    return subset
 
 num_process=8
 start=datetime.datetime.now()
 #tokenizer=BertTokenizer.from_pretrained("bert-base-uncased")
+
 tokenizer=MorphemepieceTokenizer()
 print("Start: "+str(start))
+dictionary='tokenized_data'
+isExist = os.path.exists(dictionary) 
+# Create a new directory because it does not exist
+if not isExist: 	
+  
+   os.makedirs(dictionary)
+   print("The new directory is created!")
 if __name__ == '__main__':
     with Pool(num_process) as p:
-        p.map(tokenize_text, range(1,1001))
-#for data in range(7, 9):
-    #tokenize_text(data)
+        p.map(tokenize_text, range(21))
+
 fininished=datetime.datetime.now()
 print("Finished: "+str(fininished))
 print("Total time: "+ str(fininished-start))
